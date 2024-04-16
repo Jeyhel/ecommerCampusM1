1ca656c4bb4f30fdf63dc910a0f0811c52e43c77
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-04-16 07:11:52 -0500

    release: :bookmark: checkout
    Estructura de la web
    
    hash: 95104f52690696234ad4625765e9c6b5d8fcaf7f
    
    feat: :lipstick: cambios en html y css de checkout
    
    hash: e122e5764e24c9d7fa288f3de455ac0615afbc33
    
    feat: :lipstick: Cambios css y content en checkout
    
    hash: 6646aa5e4c9ffd4579b92c17929dca51fb383f8d
    
    feat: :tada: Configuracion del ecommer
    
    Estructura de la web
    
    hash: ebcf8904d8181530696e1066bb66022e5e0866e1
    
    release: :bookmark: checkout css
    
    Estructura de la web
    
    hash: 95104f52690696234ad4625765e9c6b5d8fcaf7f
    
    feat: :lipstick: Agregue codigo y arregle 1 error
    
    hash: 46be211225be98ec3772746f1079dd00c840215b
    
    feat: :lipstick: Agregue imgs, 3variables y 1backg
    
    hash: 63345311cacd86781681b9c1861956281ead087a
    
    chore: :lipstick: Ajustes de galeria y el footer
    
    hash: f487c5970faa6abffa113cfd6f94f816a182f43c
    
    feat: :lipstick: Cambios encss y agregue contenido
    
    hash: 0eefc22a9a3dc307d5fd84707fd0424e2f84436b
    
    feat: :lipstick: Cambios css y content en checkout
    
    hash: afa20e8c6d66d3e5049d7691994654d9abef92c7
    
    feat: :lipstick: cambios en html y css de checkout
    
    hash: 723f8fb40e66cb0995f8b0aebbb160d3d6cba204
    
    chore: :lipstick: Personalización del ecommer
    
    hash: d3cb7f5cf962fd6209c0a32851fff6683443132d
    
    checkout 2

---
+++ js/checkout.js
@@ -0,0 +1,160 @@
+import { galleryCheckout, gallerycheckPrice } from "./components/gallery.js";
+import { getProductId } from "./module/detail.js";
+
+let checkout__details__gallery = document.querySelector(".checkout__details");
+
+
+document.addEventListener("DOMContentLoaded", async (e) => {
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
+    if (!sessionStorage.getItem(id)) sessionStorage.setItem(id, JSON.stringify(await getProductId({ id })));
+
+    let info = JSON.parse(sessionStorage.getItem(id));
+    checkout__details__gallery.innerHTML = await galleryCheckout();
+    section__shopping__checkout.innerHTML = await gallerycheckPrice(info);
+
+   
+    let cantidadItems = 0;
+    let totalPrecio = 0;
+
+    let details__product = document.querySelectorAll (".details__product")
+    details__product.forEach(value =>{
+
+        
+            let decreaseButton = value.querySelector("#decreaseQuantity");
+            let increaseButton = value.querySelector("#increaseQuantity");
+            let quantitySpan = value.querySelector("#quantity");
+            let Totalitems = document.querySelector("#Totalitems")
+            let precio = value.querySelector("#precio")
+        
+        
+            totalPrecio += Number((precio.textContent).replace("$",""))
+            cantidadItems += Number(quantitySpan.textContent);
+            console.log(totalPrecio)
+        
+        
+        
+            
+            decreaseButton.addEventListener('click', () => {
+                let quantity = parseInt(quantitySpan.textContent);
+                if (quantity > 1) {
+                    quantity -= 1;
+                    quantitySpan.textContent = quantity;
+                
+            
+            cantidadItems -= 1
+            totalPrecio -= Number((precio.textContent).replace("$",""))
+        
+            let Totalitems = document.querySelector("#Totalitems")
+            Totalitems.textContent = `Total(${cantidadItems} items)`;
+            let spanPrecio = document.querySelector("#spanPrecio")
+            spanPrecio.textContent = totalPrecio
+            let subPrecio = document.querySelector("#subPrecio")
+            subPrecio.textContent = totalPrecio
+            }
+        });
+            increaseButton.addEventListener('click', () => {
+                let quantity = parseInt(quantitySpan.textContent);
+                quantity += 1;
+                quantitySpan.textContent = quantity;
+            
+            
+            
+                cantidadItems += 1
+                totalPrecio += Number((precio.textContent).replace("$",""))
+        
+                let Totalitems = document.querySelector("#Totalitems")
+                Totalitems.textContent = `Total(${cantidadItems} items)`;
+                let spanPrecio = document.querySelector("#spanPrecio")
+                spanPrecio.textContent = totalPrecio
+                let subPrecio = document.querySelector("#subPrecio")
+                subPrecio.textContent = totalPrecio
+            });
+        })
+            let Totalitems = document.querySelector("#Totalitems")
+            Totalitems.textContent = `Total(${cantidadItems} items)`;
+            let spanPrecio = document.querySelector("#spanPrecio")
+            spanPrecio.textContent = totalPrecio
+            let subPrecio = document.querySelector("#subPrecio")
+            subPrecio.textContent = totalPrecio
+        
+            updatePrice(1);
+                  
+        });
+    
+
+    const updatePrice = (quantity) => {
+        let precioEntero = parseFloat(info.data.product_price.replace('$', ''));
+        let precioTotalContent = `<span id="precioTotal">$${(quantity * precioEntero).toFixed(2)}</span>`;
+        if (info.data.product_original_price !== null) {
+            let precioOriginal = parseFloat(info.data.product_original_price.replace('$', ''));
+            precioTotalContent += ` <del><sub>$${(quantity * precioOriginal).toFixed(2)}</sub></del>`;
+        }
+        precioTotal.innerHTML = precioTotalContent;
+    };
+
+
+
+let decreaseButton = document.querySelector("#decreaseQuantity");
+let increaseButton = document.querySelector("#increaseQuantity");
+let quantitySpan = document.querySelector("#quantity");
+let precioTotal = document.querySelector("#precioTotal")
+
+decreaseButton.addEventListener('click', async e => {
+    let quantity = parseInt(quantitySpan.textContent);
+if (info.data.product_price !==  null) {
+    let precioentero = parseFloat(info.data.product_price.replace('$',''))
+    if (info.data.product_original_price !== null){
+        let precioOriginal = parseFloat(info.data.product_original_price.replace('$',''));
+        if(quantity > 1){
+            quantitySpan.textContent = quantity - 1;
+        quantity = parseInt(quantitySpan.textContent);
+        precioTotal.innerHTML =/*html*/`
+            <span id= "precioTotal" >Add to Cart $${quantity * precioentero}<del><sub>$${quantity * precioOriginal}</sub></del></span>
+            `}
+        }else{
+            if(quantity > 1){
+                quantitySpan.textContent = quantity - 1;
+                quantity = parseInt(quantitySpan.textContent);
+                precioTotal.innerHTML = /*html*/`
+                <span id= "precioTotal">Add to Cart $${quantity * precioentero}</span>
+                `}
+            }
+        }else {
+            if(quantity > 1){
+                quantitySpan.textContent = quantity - 1;
+            };}
+        });
+        
+        
+        
+        increaseButton.addEventListener('click', async e => {
+            let quantity = parseInt(quantitySpan.textContent);
+            if (info.data.product_price !==  null) {
+                let precioentero = parseFloat(info.data.product_price.replace('$',''))
+                if (info.data.product_original_price !== null){
+                    let precioOriginal = parseFloat(info.data.product_original_price.replace('$',''));
+                    quantitySpan.textContent = quantity + 1;
+                    quantity = parseInt(quantitySpan.textContent);
+                    precioTotal.innerHTML =/*html*/`
+                    <span id= "precioTotal" >Add to Cart $${quantity * precioentero}<del><sub>$${quantity * precioOriginal}</sub></del></span>
+                    `}else{
+                        quantitySpan.textContent = quantity + 1;
+                        quantity = parseInt(quantitySpan.textContent);
+                        precioTotal.innerHTML = /*html*/`
+                        <span id= "precioTotal">Add to Cart $${quantity * precioentero}</span>
+                        `}
+                    }else return quantitySpan.textContent = quantity + 1;
+});
+
+                    
+                    // let informationProduct = document.querySelector("#informationProduct")
+                    // let LeerMasButton = document.querySelector("#leerMasOption");
+                    
+                    // if(LeerMasButton){
+                    //     LeerMasButton.addEventListener('click', async e => {
+                    //         let button = info.data.product_description;
+                    //         informationProduct.textContent = button;
+                    //     });
+                    // };
+                    
