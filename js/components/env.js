d20870879e0b08156bbb9f05edc01711180f9417
CampusLands <139265002+CampusLands@users.noreply.github.com>
2024-04-16 07:11:52 -0500

    release: :bookmark: index
    Estructura de la web
    
    chore: :lipstick: Cambio de imgs y delete 1footer
    
    feat: :lipstick: Agregue codigo y arregle 1 error
    
    chore: :lipstick: Cambie imagenes y un background
    
    feat: :lipstick: Agregue el footer
    
    feat: :lipstick: Agregue imgs, 3variables y 1backg
    
    chore: :lipstick: Ajustes de galeria y el footer
    
    chore: :lipstick: Personalización del ecommer
    
    feat: :tada: style index
    
    Estructura de la web
    
    chore: :lipstick: Cambio de imgs y delete 1footer
    
    chore: :lipstick: Cambie imagenes y un background
    
    feat: :lipstick: Agregue el footer
    
    feat: :lipstick: Agregue imgs, 3variables y 1backg
    
    chore: :lipstick: Ajustes de galeria y el footer
    
    feat: :lipstick: Cambios css y content en checkout
    
    chore: :lipstick: Personalización del ecommer
    
    feat: :sparkles: Arreglo en index y style
    
    index 2
    
    style: :lipstick: Cambio de color en index
    
    chore: :truck: cambio de keys

---
+++ js/components/env.js
@@ -0,0 +1,9 @@
+
+export const headers = {
+	'x-rapidapi-key': 'efba970004mshfcc5f8ee31d01f7p1b3f11jsncd15d25b2752',
+	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
+	}
+
+
+
+
---
+++ js/main.js
@@ -0,0 +1,78 @@
+import { menuListCategoryIndex } from "./components/menu.js";
+import { galleryIndex} from "./components/gallery.js";
+import { getAllProductName, getAllCategory } from "./module/app.js";
+import { getAllInicio } from "./module/app.js";
+
+
+let input__search = document.querySelector("#input__search");
+let main__article = document.querySelector(".main__article");
+let nav__ul = document.querySelector(".nav__ul");
+
+addEventListener("DOMContentLoaded", async e=>{
+    if(!localStorage.getItem("getAllCategory")) localStorage.setItem("getAllCategory", JSON.stringify(await getAllCategory()));
+    nav__ul.innerHTML = await menuListCategoryIndex(JSON.parse(localStorage.getItem("getAllCategory")));  
+    let res = await getAllInicio()
+    main__article.innerHTML = galleryIndex (res,"fashion")
+
+    let llaves = Object.keys(sessionStorage);
+    let listauwu = 0;
+    llaves.forEach(llave => {
+        if((JSON.parse(sessionStorage.getItem(llave))).holi){
+            listauwu++;
+        }
+    })
+    console.log(listauwu);
+    let index__contador = document.querySelector("#index__contador");
+    index__contador.textContent = listauwu
+
+    
+})
+
+input__search.addEventListener("change", async e => {
+    let params = new URLSearchParams(location.search);
+    let data = { search : e.target.value, id: params.get('id')}
+    input__search.value = null;
+    let res = await getAllProductName(data)
+    main__article.innerHTML = galleryIndex(res, params.get('id'));
+    
+});
+// addEventListener("DOMContentLoaded", async e =>{
+        //     let data = await getAllCategory();
+        //     nav__ul.innerHTML = await menuListCategoryIndex(data);  
+        
+
+        
+        // let searchProducts = async e => {
+        
+        //     let params = new URLSearchParams(location.search);
+        //     let dataSearch = { search : e.target.value, id: params.get('id')}
+        //     input__search.value = null;
+        //     let res = ""
+        //     if(input__search.dataset.opc == "random"){
+        //         res = await getAllProductRandom({})
+        //         delete input__search.dataset.opc
+        //         history.pushState(null, "", "?id=aps");
+        //         console.log(dataSearch);
+        //     }
+        //     else {
+        //         res = await getAllProductName(dataSearch)
+        //         console.log(dataSearch);
+        //     }
+        //     console.log(res);
+        //     main__article.innerHTML = galleryIndex(res, params.get('id'));
+        
+        //     let {data: {products}} = res;
+        //     let asin = products.map(value => {return {id: value.asin}});
+        
+        //     let proceso = new Promise(async(resolve, reject)=>{
+        //         for (let i = 0; i < asin.length; i++) {
+        //             if(localStorage.getItem(asin[i].id)) continue;
+        //             let data = await getProductId(asin[i])
+        //             localStorage.setItem(asin[i].id, JSON.stringify(data))
+        //         }
+        //         resolve({message: "Datos buscados correctamente" });
+        //     })
+        //     Promise.all([proceso]).then(res => {console.log(res);})
+        
+        // }
+// })
