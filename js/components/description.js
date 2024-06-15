542f2789cfc1452c406bdba4953053b6eb7dc610
Jeyhel <jeyhelangelina0212@gmail.com>
2024-06-15 14:35:08 -0500

    feat: :sparkles: Funcionalidad de la descripcion y del aumento y disminucion del producto

---
+++ js/components/description.js
@@ -0,0 +1,18 @@
+export const descripDetails = async({data: dataUpdate} = res) => {
+    let createDescripHTML = async() => {
+        let description = await dataUpdate.product_description;
+        let trunDescription = description;
+    
+        if (description.length > 150) {
+            trunDescription = description.substring(0, 150) + '... <strong id = "leerMasOption"> Leer más.</strong>';
+        }
+        return `${trunDescription}`;
+        
+    }
+    
+    return /*html*/`
+    <article class="product__information">
+            <p id = "informationProduct">${await createDescripHTML()}</p>
+    </article>
+    `
+};
