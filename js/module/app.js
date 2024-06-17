adc1ff2c1aa2df4e49d835ffea5950e855a0df9c
Jeyhel <160855981+Jeyhel@users.noreply.github.com>
2024-06-17 09:12:44 -0500

    feat: :building_construction: Proceso de random de la galeria index
    Se modifico el input del archivo index.html

--- js/module/app.js
+++ js/module/app.js
@@ -24,3 +24,26 @@ export const getAllCategory = async()=>{
     let data = res.json();
     return data;
 }
+
+export const getAllProductRandom = async ()=>({
+    query="blush",
+    page="2",
+    category_id="make up",
+    min_price=100,
+    max_price=300,
+    brand=["rare beauty", "dior", "chanel"]})=>{
+    console.log ("Cargando dato.........");
+    page = Math.random()*(page/20);
+    page = parseInt(Math.round(page));
+    if(!page) page = 1;
+    const url = `https://real-time-amazon-data.p.rapidapi.com/search?query=${query}blush&page${page}=2&country=US&sort_by=RELEVANCE&category_id=${category_id}make%20up&min_price=${min_price}100&max_price=${max_price}300&product_condition=NEW&brand=${brand.join("%2C")}rare%20beauty%2Cdior%2Cchanel`;
+    const options = {
+	method: 'GET',
+	headers
+	
+    };
+    let res = await fetch(url, options);
+    let data = res.json();
+    return data;
+
+    }
