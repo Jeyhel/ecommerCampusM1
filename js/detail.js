14d27d1a77b48e3cc088306a67f27009353ed95d
Jeyhel <jeyhelangelina0212@gmail.com>
2024-06-17 17:35:06 -0500

    feat: :sparkles: Funcionalidad de aumento del precio al aumentar el producto
    Ademas, se intento cambiar el color de las imagnes al seleccionarse

--- js/detail.js
+++ js/detail.js
@@ -20,31 +20,109 @@ addEventListener("DOMContentLoaded", async(e)=>{
     main__section__description.innerHTML = await descripDetails(info);
     footer__ul.innerHTML = await priceDetails(info);
 
-    
-        let decreaseButton = document.querySelector("#decreaseQuantity");
-        let increaseButton = document.querySelector("#increaseQuantity");
-        let quantitySpan = document.querySelector("#quantity");
-        
-        decreaseButton.addEventListener('click', async e => {
-        let quantity = parseInt(quantitySpan.textContent);
+
+    let decreaseButton = document.querySelector("#decreaseQuantity");
+    let increaseButton = document.querySelector("#increaseQuantity");
+    let quantitySpan = document.querySelector("#quantity");
+    let precioTotal = document.querySelector("#precioTotal")
+
+    decreaseButton.addEventListener('click', async e => {
+    let quantity = parseInt(quantitySpan.textContent);
+    if (info.data.product_price !==  null) {
+    let precioentero = parseFloat(info.data.product_price.replace('$',''))
+    if (info.data.product_original_price !== null){
+        let precioOriginal = parseFloat(info.data.product_original_price.replace('$',''));
         if(quantity > 1){
             quantitySpan.textContent = quantity - 1;
-        };
-        });
+            quantity = parseInt(quantitySpan.textContent);
+            precioTotal.innerHTML =/*html*/`
+                <span id= "precioTotal" >Add to Cart $${quantity * precioentero}<del><sub>$${quantity * precioOriginal}</sub></del></span>
+                `}
+            }else{
+                    if(quantity > 1){
+                    quantitySpan.textContent = quantity - 1;
+                    quantity = parseInt(quantitySpan.textContent);
+                    precioTotal.innerHTML = /*html*/`
+                        <span id= "precioTotal">Add to Cart $${quantity * precioentero}</span>
+                        `}
+            }
+            }else {
+                if(quantity > 1){
+                quantitySpan.textContent = quantity - 1;
+            };}
+            });
         
-        increaseButton.addEventListener('click', async e => {
-        let quantity = parseInt(quantitySpan.textContent);
-        quantitySpan.textContent = quantity + 1;
-        });
-    
-        let LeerMasButton = document.querySelector("#leerMasOption");
-        let informationProduct = document.querySelector("#informationProduct")
-    
-        LeerMasButton.addEventListener('click', async e => {
-            let description = info.data.product_description;
-            informationProduct.textContent = description;
-        });
+   
+
+    increaseButton.addEventListener('click', async e => {
+            let quantity = parseInt(quantitySpan.textContent);
+            if (info.data.product_price !==  null) {
+            let precioentero = parseFloat(info.data.product_price.replace('$',''))
+            if (info.data.product_original_price !== null){
+                let precioOriginal = parseFloat(info.data.product_original_price.replace('$',''));
+                quantitySpan.textContent = quantity + 1;
+                quantity = parseInt(quantitySpan.textContent);
+                precioTotal.innerHTML =/*html*/`
+                    <span id= "precioTotal" >Add to Cart $${quantity * precioentero}<del><sub>$${quantity * precioOriginal}</sub></del></span>
+            `}else{
+                quantitySpan.textContent = quantity + 1;
+                quantity = parseInt(quantitySpan.textContent);
+                precioTotal.innerHTML = /*html*/`
+                <span id= "precioTotal">Add to Cart $${quantity * precioentero}</span>
+                `}
+            }else return quantitySpan.textContent = quantity + 1;
+});
+
+
+let s = document.querySelector("#s");
+let m = document.querySelector("#m");
+let l = document.querySelector("#l");
+let xl = document.querySelector("#xl");
+
+let images = [s, m, l, xl];
+let imgPaths = {
+    s: "../storage/img/sizeS.new.svg",
+    m: "../storage/img/sizeM.new.svg",
+    l: "../storage/img/sizeL.new.svg",
+    xl: "../storage/img/sizeXL.new.svg"
+};
+
+let originalPaths = {
+    s: "../storage/img/sizeS.svg",
+    m: "../storage/img/sizeM.svg",
+    l: "../storage/img/sizeL.svg",
+    xl: "../storage/img/sizeXL.svg"
+};
+
+function deselectAll() {
+    images.forEach(image => {
+        image.src = originalPaths[image.id];
     });
+}
+
+s.addEventListener("click", (e) => {
+    deselectAll();
+    s.src = imgPaths.s;
+});
+
+m.addEventListener("click", (e) => {
+    deselectAll();
+    m.src = imgPaths.m;
+});
+
+l.addEventListener("click", (e) => {
+    deselectAll();
+    l.src = imgPaths.l;
+});
+
+xl.addEventListener("click", (e) => {
+    deselectAll();
+    xl.src = imgPaths.xl;
+});
+});
+
+
+
     // let {data} = res;
     // let {
     //     category_path,
@@ -58,7 +136,3 @@ addEventListener("DOMContentLoaded", async(e)=>{
     //     ...dataUpdate
     // } = data;
     // console.log(dataUpdate);
-
-
-
-
