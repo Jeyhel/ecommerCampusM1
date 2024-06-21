3abf1e2724e343b36602776b677919600fd27281
Jeyhel <160855981+Jeyhel@users.noreply.github.com>
2024-06-21 09:22:31 -0500

    fix: :bug: Cambio en las keys

--- js/components/env.js
+++ js/components/env.js
@@ -1,6 +1,6 @@
 
 export const headers = {
-	'x-rapidapi-key': 'f2876eca3fmshd345741c8249687p102fd9jsneaed0dacea88',
+	'x-rapidapi-key': '6a0d031a54mshef165cfefd66779p145d02jsn9368dc51cff0',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
 
