690e4892af47c3c0163edf43c9b068f0bc58faaf
Jeyhel <jeyhelangelina0212@gmail.com>
2024-06-24 17:29:45 -0500

    style: :lipstick: Se css de loading de detail

--- js/module/detail.js
+++ js/module/detail.js
@@ -1,6 +1,6 @@
 import { headers } from "../components/env.js";
 
-
+document.querySelector(".carga").style.display = "block";
 export const getProductId = async({id:idCategory})=>{
     console.log("Esperando .......");
     const url = `https://real-time-amazon-data.p.rapidapi.com/product-details?asin=${idCategory}&country=US`;
@@ -10,5 +10,6 @@ export const getProductId = async({id:idCategory})=>{
     };
     let res = await fetch(url, options);
     let data = res.json();
+    document.querySelector(".carga").style.display = "none";
     return data;
 }
