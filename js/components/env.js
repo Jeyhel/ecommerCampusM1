658b4a4090d680bce4dcfc72ca02dd6a5efbf36a
Jeyhel <160855981+Jeyhel@users.noreply.github.com>
2024-06-24 07:44:16 -0500

    chore: :truck: Cambio de keys

--- js/components/env.js
+++ js/components/env.js
@@ -1,6 +1,6 @@
 
 export const headers = {
-	'x-rapidapi-key': '6a0d031a54mshef165cfefd66779p145d02jsn9368dc51cff0',
+	'x-rapidapi-key': 'bbab4db3c5msh3a5cf296e890e77p124747jsnf400745662fd',
 	'x-rapidapi-host': 'real-time-amazon-data.p.rapidapi.com'
 }
 
